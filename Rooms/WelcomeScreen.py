f24467e76e4b340b82f229e22d12ce17406cf540
WillsDad007 <william.smith8423@icloud.com>
2026-07-27 13:15:53 +1000

    Update WelcomeScreen.py

--- Rooms/WelcomeScreen.py
+++ Rooms/WelcomeScreen.py
@@ -5,4 +5,4 @@ class WelcomeScreen(Level):
     Initial Screen for Game
     """
     def __init__(self, screen, joysticks):
-        Level._init__(self, screen, joysticks)
+        Level.__init__(self, screen, joysticks)
