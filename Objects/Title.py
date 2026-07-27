bf94bca06988111f7bac48b9305a2b83309be065
WillsDad007 <william.smith8423@icloud.com>
2026-07-27 13:23:22 +1000

    Fixed missing colon

--- Objects/Title.py
+++ Objects/Title.py
@@ -4,10 +4,9 @@ class Title(RoomObject):
     """
     The object for displaying the title
     """
-    def __init__(self, room, x, y)
+    def __init__(self, room, x, y):
         RoomObject.__init__(self, room, x, y)
 
         # set image
         image = self.load_image("Title.png")
         self.set_image(image,800,350)
-        
