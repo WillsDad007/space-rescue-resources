a8264fe3528c805ae20e7ccb47dd668c0655e6a1
WillsDad007 <william.smith8423@icloud.com>
2026-07-31 15:04:02 +1000

    Added Asteroids

---
+++ Objects/Asteroid.py
@@ -0,0 +1,16 @@
+from GameFrame import RoomObject
+
+class Asteroid(RoomObject):
+    """
+    A class for Zorks danerous obstacles
+    """
+    def __init__(self, room, x, y):
+        """
+        Initialise the asteroid object
+        """
+        # include attributes and methods from RoomObject
+        RoomObject.__init__(self, room, x, y)
+
+        # set image
+        image = self.load_image("asteroid.png")
+        self.set_image(image,50,49)
