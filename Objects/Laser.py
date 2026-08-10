20cc7f2cdbf13720c2cbcb944f8e69be7c58e2f1
WillsDad007 <william.smith8423@icloud.com>
2026-08-10 13:50:05 +1000

    Added sounds

--- Objects/Laser.py
+++ Objects/Laser.py
@@ -38,8 +38,10 @@ class Laser(RoomObject):
         Handles laser collisions with other registered objects
         """
         if other_type == "Asteroid":
+            self.room.asteroid_shot.play()
             self.room.delete_object(other)
             self.room.score.update_score(5)
         elif other_type == "Astronaut":
+            self.room.astronaut_shot.play()
             self.room.delete_object(other)
             self.room.score.update_score(-10)
